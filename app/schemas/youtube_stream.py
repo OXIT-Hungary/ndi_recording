6941a0db01642d43ef7fe19ab2664b79faf077ea
kristof-oxit <166016826+kristof-oxit@users.noreply.github.com>
2025-04-01 16:25:18 +0200

    Adding Youtube Streaming feature
    * Initial commit on new streaming
    
    * Refactored code after testing
    
    * Content not for Kids and QoL changes
    
    * Added category selection

---
+++ app/schemas/youtube_stream.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class YoutubeStreamSchedule(BaseModel):
+    title: str
+    description: str
+    privacy_status: str
+    start_time: datetime
+    end_time: datetime
+    category: str
